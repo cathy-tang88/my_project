7b81cda27760b31f1ae33a03e1871f462228cc78
cathy-tang88 <tang.jingxiao@163.com>
2012-12-29 22:24:21 +0800

    nainai

--- src/tjx/trs/run/Selectuser.java
+++ src/tjx/trs/run/Selectuser.java
@@ -1,47 +1,49 @@
 package tjx.trs.run;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import tjx.trs.util.CollectionUtil;
 
 import love.cq.util.IOUtil;
 
 public class Selectuser {
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = IOUtil.getReader("D:\\”Ô¡œ\\SogouQ\\filter.txt", "UTF-8");
+		HashMap<String, Integer> hsmap=new HashMap<String, Integer>();
 		String temp = null;
-        HashMap<String, HashMap<String, Integer>> tagHashMap=new HashMap<String, HashMap<String, Integer>> ();
 		while ((temp = reader.readLine()) != null) {
 			String[] split = temp.split("\t");
-			String id=split[1];
-			String queryString=split[2];
-			if(tagHashMap.containsKey(id))
-			{
-				HashMap<String,Integer> hasMap=tagHashMap.get(id);
-				
-				if(hasMap.containsKey(queryString)){
-				hasMap.put(queryString, hasMap.get(queryString)+1);
-				}
-				else{
-					hasMap.put(queryString, 1);
-				}
-				tagHashMap.put(id, hasMap);
+			String query = split[2];
+			if(hsmap.containsKey(query)){
+				hsmap.put(query,hsmap.get(query)+1);
 			}
 			else{
-				HashMap<String , Integer> noMap=new HashMap<String, Integer>();
-				if(noMap.containsKey(queryString)){
-					noMap.put(queryString, noMap.get(queryString)+1);
-					}
-					else{
-						noMap.put(queryString, 1);
-					}
-				tagHashMap.put(id, noMap);
+				hsmap.put(query,1);
 			}
-			
 		}
-		
-		System.out.println(tagHashMap);
+		Iterator it = hsmap.entrySet().iterator();
+		BufferedWriter writer=new BufferedWriter(new OutputStreamWriter(new FileOutputStream("sort.txt",false)));
+		while (it.hasNext()) {
+			Map.Entry<String, Integer> entry = (Map.Entry<String, Integer>) it.next();
+			String word = entry.getKey();
+			Integer number=entry.getValue();
+			writer.write(word+"\t"+number+"\r\n");
+		}
+		writer.close();
 	}
+	
 }
