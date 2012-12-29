81a08be8ddf645e741e57a9eb2b43a8f30257447
cathy-tang88 <tang.jingxiao@163.com>
2012-12-29 21:30:39 +0800

    so

--- src/tjx/trs/run/QueryDemo.java
+++ src/tjx/trs/run/QueryDemo.java
@@ -1,6 +1,8 @@
 package tjx.trs.run;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -15,69 +17,74 @@ import tjx.trs.util.CollectionUtil;
 
 public class QueryDemo {
 	public static void main(String[] args) throws Exception {
-		filter("aa");
-		BufferedReader reader = IOUtil.getReader("D:\\语料\\SogouQ\\filter.txt", "UTF-8");
-		String temp = null;
-		HashMap<String, Integer> hMap = new HashMap<String, Integer>();
-		while ((temp = reader.readLine()) != null) {
-			String[] split = temp.split("\t");
-			temp = split[2];
+		
+		
+		
+		FileOutputStream fos = new FileOutputStream("D:\\语料\\SogouQ\\filter.txt");
+		File[] files = new File("D:\\语料\\SogouQ\\all").listFiles();
+		for (int i = 0; i < files.length; i++) {
+			if (!files[i].canRead() || !files[i].getName().endsWith("sogou")) {
+				continue;
+			}
+			BufferedReader reader = IOUtil.getReader(new FileInputStream(files[i]), "GBK");
+			String temp = null;
 
-			GetWord gWord = new GetWord(forest, temp);
-			while ((temp = gWord.getFrontWords()) != null) {
-				if (hMap.containsKey(temp)) {
-					hMap.put(temp, hMap.get(temp) + 1);
-				} else {
-					hMap.put(temp, 1);
+			while ((temp = reader.readLine()) != null) {
+				String[] split = temp.split("\t");
+				if (filter(split[2])) {
+					fos.write(temp.getBytes());
+					fos.write("\n".getBytes());
 				}
 			}
 		}
+		fos.flush();
+		fos.close();
+	}
+	
+	
+	public static  void clearnWord()throws Exception{
+		filter("aa");
+		//	BufferedReader reader = IOUtil.getReader("D:\\语料\\SogouQ\\filter.txt", "UTF-8");
+			BufferedReader reader = IOUtil.getReader("D:\\语料\\SogouQ\\all.txt", "UTF-8");
+			String temp = null;
+			HashMap<String, Integer> hMap = new HashMap<String, Integer>();
+			while ((temp = reader.readLine()) != null) {
+				String[] split = temp.split("\t");
+				temp = split[2];
 
-		System.out.println(hMap.size());
-		List<Entry<String, Integer>> sortMapByValue = CollectionUtil.sortMapByValue(hMap, 1);
-		HashSet<String> hs = new HashSet<String>();
-		for (int i = 0; i < 200; i++) {
-			hs.add(sortMapByValue.get(i).getKey()) ;
-		}
-		
-		
-		
-		reader = IOUtil.getReader("data\\csdn_class.txt", "UTF-8");
-		
-		 FileOutputStream fos = new FileOutputStream("data\\csdn_class.new.txt");
-		while((temp=reader.readLine())!=null){
-			if(hs.contains(temp.split("\t")[0])){
-				continue ;
-			}else{
-				fos.write(temp.getBytes()) ;
-				fos.write("\n".getBytes()) ;
+				GetWord gWord = new GetWord(forest, temp);
+				while ((temp = gWord.getFrontWords()) != null) {
+					if (hMap.containsKey(temp)) {
+						hMap.put(temp, hMap.get(temp) + 1);
+					} else {
+						hMap.put(temp, 1);
+					}
+				}
 			}
-		}
-		fos.flush() ;
-		fos.close() ;
-		
-		
-		// FileOutputStream fos = new
-		// FileOutputStream("D:\\语料\\SogouQ\\filter.txt");
-		// File[] files = new File("D:\\语料\\SogouQ\\split").listFiles();
-		// for (int i = 0; i < files.length; i++) {
-		// if (!files[i].canRead() || !files[i].getName().endsWith("sogou")) {
-		// continue;
-		// }
-		// BufferedReader reader = IOUtil.getReader(new
-		// FileInputStream(files[i]), "GBK");
-		// String temp = null;
-		//
-		// while ((temp = reader.readLine()) != null) {
-		// String[] split = temp.split("\t");
-		// if (filter(split[2])) {
-		// fos.write(temp.getBytes());
-		// fos.write("\n".getBytes());
-		// }
-		// }
-		// }
-		// fos.flush();
-		// fos.close();
+
+			System.out.println(hMap.size());
+			List<Entry<String, Integer>> sortMapByValue = CollectionUtil.sortMapByValue(hMap, 1);
+			HashSet<String> hs = new HashSet<String>();
+			for (int i = 0; i < 300; i++) {
+				System.out.println(sortMapByValue.get(i));
+				//hs.add(sortMapByValue.get(i).getKey()) ;
+			}
+			
+			
+			
+			reader = IOUtil.getReader("data\\csdn_class.txt", "UTF-8");
+			
+			 FileOutputStream fos = new FileOutputStream("data\\csdn_class.new.txt");
+			while((temp=reader.readLine())!=null){
+				if(hs.contains(temp.split("\t")[0])){
+					continue ;
+				}else{
+					fos.write(temp.getBytes()) ;
+					fos.write("\n".getBytes()) ;
+				}
+			}
+			fos.flush() ;
+			fos.close() ; 
 	}
 
 	private static Forest forest = null;
@@ -85,13 +92,16 @@ public class QueryDemo {
 	public static boolean filter(String query) throws Exception {
 		if (forest == null) {
 			forest = Library.makeForest("data/csdn_class.txt");
+			//forest = Library.makeForest(IOUtil.getReader("data/计算机与自动化.txt", "UTF-8")) ;
 		}
 		GetWord gw = new GetWord(forest, query);
 		int size = 0;
-		while ((gw.getFrontWords()) != null) {
-			size++;
+		String temp = null ;
+		while ((temp=gw.getFrontWords()) != null) {
+			
+			size+=temp.length();
 		}
-		if (size > 2) {
+		if (size > 5) {
 			return true;
 		}
 		return false;
---
+++ src/tjx/trs/run/Selectuser.java
@@ -0,0 +1,41 @@
+package tjx.trs.run;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import love.cq.util.IOUtil;
+
+public class Selectuser {
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = IOUtil.getReader("D:\\”Ô¡œ\\SogouQ\\filter.txt", "UTF-8");
+		String temp = null;
+        HashMap<String, HashMap<String, Integer>> tagHashMap=new HashMap<String, HashMap<String, Integer>> ();
+		while ((temp = reader.readLine()) != null) {
+			String[] split = temp.split("\t");
+			String id=split[1];
+			
+			if(tagHashMap.containsKey(id))
+			{
+				HashMap<String,Integer> hasMap=tagHashMap.get(id);
+				String queryString=split[2];
+				if(hasMap.containsKey(queryString)){
+				hasMap.put(queryString, hasMap.get(queryString)+1);
+				}
+				else{
+					hasMap.put(queryString, 1);
+				}
+				tagHashMap.put(id, hasMap);
+			}
+			else{
+				HashMap<String , Integer> noMap=new HashMap<String, Integer>();
+				
+				tagHashMap.put(id, noMap);
+			}
+			
+		}
+		System.out.println(tagHashMap);
+	}
+}
