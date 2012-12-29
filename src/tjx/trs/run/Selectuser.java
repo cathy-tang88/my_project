19292dc54d9bf3cc97db9a5ab3803878a919bd8c
ansj <ansj-sun@163.com>
2012-12-29 22:34:32 +0800

    词频统计

--- src/tjx/trs/run/Selectuser.java
+++ src/tjx/trs/run/Selectuser.java
@@ -5,43 +5,53 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
 
+import org.ansj.util.CollectionUtil;
+
+import love.cq.domain.Forest;
+import love.cq.library.Library;
+import love.cq.splitWord.GetWord;
 import love.cq.util.IOUtil;
 
 public class Selectuser {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader reader = IOUtil.getReader("D:\\语料\\SogouQ\\filter.txt", "UTF-8");
 		String temp = null;
-        HashMap<String, HashMap<String, Integer>> tagHashMap=new HashMap<String, HashMap<String, Integer>> ();
+		HashMap<String, Integer> hm = new HashMap<String, Integer>();
+		Forest forest = Library.makeForest("data/csdn_class.txt");
+		GetWord getWord = null;
+		int count = 0;
 		while ((temp = reader.readLine()) != null) {
 			String[] split = temp.split("\t");
-			String id=split[1];
-			String queryString=split[2];
-			if(tagHashMap.containsKey(id))
-			{
-				HashMap<String,Integer> hasMap=tagHashMap.get(id);
-				
-				if(hasMap.containsKey(queryString)){
-				hasMap.put(queryString, hasMap.get(queryString)+1);
+			count++;
+			getWord = new GetWord(forest, split[2]);
+			while ((temp = getWord.getFrontWords()) != null) {
+				if (hm.containsKey(temp)) {
+					hm.put(temp, hm.get(temp) + 1);
+				} else {
+					hm.put(temp, 2);
 				}
-				else{
-					hasMap.put(queryString, 1);
-				}
-				tagHashMap.put(id, hasMap);
 			}
-			else{
-				HashMap<String , Integer> noMap=new HashMap<String, Integer>();
-				if(noMap.containsKey(queryString)){
-					noMap.put(queryString, noMap.get(queryString)+1);
-					}
-					else{
-						noMap.put(queryString, 1);
-					}
-				tagHashMap.put(id, noMap);
-			}
-			
+
 		}
 		
-		System.out.println(tagHashMap);
+		//补录为0的东西
+		BufferedReader reader2 = IOUtil.getReader("data/csdn_class.txt", "UTF-8") ;
+		while((temp=reader2.readLine())!=null){
+			String[] split = temp.split("\t");
+			if(!hm.containsKey(split[0])){
+				hm.put(split[0], 1) ;
+			}
+		}
+		
+		System.out.println("总数" + count);
+		StringBuilder sb = new StringBuilder();
+		List<Entry<String, Integer>> sortMapByValue = CollectionUtil.sortMapByValue(hm, 1) ;
+		for (Entry<String, Integer> entry : sortMapByValue) {
+			sb.append(entry.getKey()+"\t"+entry.getValue()) ;
+			sb.append("\n") ;
+		}
 	}
 }
